25325a6d01c6301160ffcc87556d6e7954ad341f
wellyngtonF <wellyngton61@gmail.com>
2021-10-18 20:54:28 -0300

    auth validation

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,6 +1,17 @@
+import { IsNotEmpty, MinLength, IsEmail, IsBoolean } from 'class-validator'
 export class UserDto {
+	@IsNotEmpty()
 	readonly name: string
+
+	@IsNotEmpty()
+	@IsEmail()
 	readonly email: string
+
+	@IsNotEmpty()
+	@MinLength(6)
 	readonly password: string
-	readonly admin: string
+
+	@IsNotEmpty()
+	@IsBoolean()
+	readonly admin: boolean
 }
